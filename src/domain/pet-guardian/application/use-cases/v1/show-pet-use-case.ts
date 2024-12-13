d2ea2b2d5e30f36698ae01b2b5be41f08fa5890e
Lubni Morais <lubni.morais@gmail.com>
2024-12-13 18:33:00 -0300

    feat: show pets

--- src/domain/pet-guardian/application/use-cases/v1/show-pet-use-case.ts
+++ src/domain/pet-guardian/application/use-cases/v1/show-pet-use-case.ts
@@ -1,1 +1,11 @@
-export class ShowPetUseCase {}
+import { inject, injectable } from 'tsyringe';
+
+import { IPetsRepository } from '../../repositories/v1/pets-repository';
+
+@injectable()
+export class ShowPetUseCase {
+  constructor(
+    @inject('PetsRepository')
+    private petsRepository: IPetsRepository,
+  ) {}
+}
